d3e3a3dc0332e7a2d9661bbd6837b61e826eebe0
Jing Tang <jtangesq@Jings-MBP.lan>
2021-07-06 17:49:07 -0400

    updated markers and circles

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,7 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 // Create the map object with a center and zoom level.
 // anothe way to create map object 
 // let map = L.map("mapid", {
@@ -13,7 +13,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //add map tile layers that will be the background of our map
 // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         accessToken: API_KEY
@@ -21,3 +21,19 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     
     // Then we add our 'graymap' tile layer to the map.
     streets.addTo(map);
+
+//var marker = L.marker([51.5, -0.09]).addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//Add a Circle to the Map--The circle() function will place a circle on the map at the given coordinates.
+// assign a value to the radius key in the circle 
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+ }).addTo(map);
+
+ //we can create a circle using the circleMarker() function.
+
